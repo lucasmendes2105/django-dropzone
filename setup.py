e9493567ac7e527b008102d42fa3bf2be9d7ddb3
Lucas Mendes <lucasmendes2105@gmail.com>
2022-06-17 16:06:16 -0300

    0.5.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-dropzone',
-    version='0.4.1',
+    version='0.5.1',
     packages=['dropzone'],
     include_package_data=True,
     license='MIT License',  # example license
